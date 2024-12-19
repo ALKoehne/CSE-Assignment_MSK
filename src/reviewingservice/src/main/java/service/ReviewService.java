f5f6f4aa8b8468fcc5374de6d3af0a5720491422
Stefan-Marian Mihalcea <s.mihalcea@campus.tu-berlin.de>
2024-12-19 14:14:33 +0100

    java comments

--- src/reviewingservice/src/main/java/service/ReviewService.java
+++ src/reviewingservice/src/main/java/service/ReviewService.java
@@ -16,13 +16,15 @@ import java.util.List;
 
 
 public class ReviewService extends ProductReviewServiceGrpc.ProductReviewServiceImplBase {
-    //sendet
+    //Grpc methode um AddReview Request zu erfüllen. Braucht eine Request, ruft dann in der Methode die addResponseBuilder Methode auf und ist dann vertig
     @Override
     public void addReview(service.ProductReviewServiceOuterClass.AddReviewRequest request,
                           io.grpc.stub.StreamObserver<service.ProductReviewServiceOuterClass.AddReviewResponse> responseObserver) throws IOException, ParseException {
         responseObserver.onNext(addResponseBuilder(request));
         responseObserver.onCompleted();
     }
+
+    //Methode die da ist um die AddReview Methode zu unterstützen. Braucht einen AddReviewRequest, ruft dann die Methode addReviewRating mit den Werten aus request und sendet dann true oder false wenn es erflogreich war. 
     public ProductReviewServiceOuterClass.AddReviewResponse addResponseBuilder (service.ProductReviewServiceOuterClass.AddReviewRequest request) throws IOException, ParseException {
         addReviewAndRating(request.getProdid(),request.getReview(),request.getRating());
         String [] reviews = getProdReviews(request.getProdid());
@@ -38,12 +40,17 @@ public class ReviewService extends ProductReviewServiceGrpc.ProductReviewService
         return response;
     }
 
+
+    //Grpc Methode die ,die getReview Request erfüllt indem es mithilfe von getReviewsResponseBuilder Methode eine Response liefert
     @Override
     public void getReviews(service.ProductReviewServiceOuterClass.GetReviewsRequest request,
                            io.grpc.stub.StreamObserver<service.ProductReviewServiceOuterClass.GetReviewsResponse> responseObserver) throws IOException, ParseException {
         responseObserver.onNext(getReviewsResponseBuilder(request));
         responseObserver.onCompleted();
     }
+
+
+    //Methode, die in der json nach dem Product in der json Datein mithilfe der ProdId sucht, die in der Request beinhaltet ist, und itteriert durch die Reviews Array und Rating array und die in der Response speichert und dann returned.
     public ProductReviewServiceOuterClass.GetReviewsResponse getReviewsResponseBuilder (ProductReviewServiceOuterClass.GetReviewsRequest request) throws IOException, ParseException {
        ProductReviewServiceOuterClass.GetReviewsResponse response = ProductReviewServiceOuterClass.GetReviewsResponse.newBuilder().build();
         for (int i = 0; i < (getProdReviews(request.getProdid()).length);i++){
@@ -54,6 +61,8 @@ public class ReviewService extends ProductReviewServiceGrpc.ProductReviewService
         }
         return response;
     }
+
+    //Grpc Methode ,die bei einer GetAverageRatingRequest die Average ratings und totale Anzahl an Ratings von einen Product bei einer Request zurücksendet mit hilfe von der getAverageRatingResponseBuilder methode.
     @Override
     public void getAverageRating(service.ProductReviewServiceOuterClass.GetAverageRatingRequest request,
                                  io.grpc.stub.StreamObserver<service.ProductReviewServiceOuterClass.GetAverageRatingResponse> responseObserver) throws IOException, ParseException {
@@ -61,12 +70,16 @@ public class ReviewService extends ProductReviewServiceGrpc.ProductReviewService
         responseObserver.onCompleted();
     }
 
+    //Methode die, die average Rating und Anzahl der Ratings als Response returned mit hilfe von getAvarageProdRating Methode.
     public ProductReviewServiceOuterClass.GetAverageRatingResponse getAverageRatingResponseBuilder (ProductReviewServiceOuterClass.GetAverageRatingRequest request) throws IOException, ParseException {
         ProductReviewServiceOuterClass.GetAverageRatingResponse response = ProductReviewServiceOuterClass.GetAverageRatingResponse.newBuilder().setAverageRating(getAvarageProdRating(request.getProdid())[0]).setReviewCount((int) getAvarageProdRating(request.getProdid())[1]).build();
         return response;
     }
 
+    //Grpc Methoden vertig
 
+
+    //Methodem die Eine ProdId braucht, sich dann den json file anschaut , nach dem Product filtert mit der richtigen ID, itteriert dann den Rating array uns speichert es in einen temporären array mit dem mann dann die länge berechnen kann und alle Reviews zusammen zählt, returned dann einen Long array wo der erst Element immer der Avrg Rating ist und der zweite immer die totale Anzahl an den Reviews
     public long[]  getAvarageProdRating (String prodid) throws IOException, ParseException {
         Path path = Paths.get("demo.json");
         Object json = new JSONParser().parse(new FileReader(path.toRealPath().toString() ));
@@ -84,6 +97,7 @@ public class ReviewService extends ProductReviewServiceGrpc.ProductReviewService
         return new long[]{Avgrating/prodt.toArray().length,  prodt.toArray().length};
     }
 
+    //Methode ,die Reviews in der json added, braucht dafür die Prod Id, das Review und das Rating. Da jsons immutable sind, nach mein verständnis, kann man das File nicht einfach edditen, sonder man muss eine neue erzeugen, deswegen bin ich als nächstes so vorgegangen: Schaut sich den File an, speichert dann die schon existirende Reviews und Rating von den genauen Product in zwei Arrays, added dann da nochmal die neuen Reviews und Ratings. Dann wird eine neue json Datei mit der selbe FileName und Filepath, coopiert dann alle Producte aus den alten json Datei, ausßer die, die die gegeben Id hat. Die wird dann aus den temporären Arrays, die in der Methode erzeugt wurden und dann in der neue
     private void addReviewAndRating(String prodID, String review, long rating) throws IOException, ParseException {
         Path path = Paths.get("demo.json");
 
@@ -124,10 +138,11 @@ public class ReviewService extends ProductReviewServiceGrpc.ProductReviewService
         FileWriter writer = new FileWriter(path.toRealPath().toString(),false);
         writer.write(jsonObject.toString());
         writer.close();
-        //find product and add review
+        
 
     }
 
+    // Methode, die eine ProdId braucht, such dann in dem File nach den genauen Product mitihilfe von der Id, speichert sie dann in einen temporären JsonArray, die dann in einen String Array gespeichert wird und zurückgegeben.
     public String[] getProdReviews(String prodid) throws IOException, ParseException {
         Path path = Paths.get("demo.json");
         Object json = new JSONParser().parse(new FileReader(path.toRealPath().toString() ));
@@ -144,7 +159,7 @@ public class ReviewService extends ProductReviewServiceGrpc.ProductReviewService
         }
         return reviews;
     }
-    //Fragt nach Product id, gibt alle ratings des Products zurück
+    //Methode, die eine Prod Id Braucht, sich dann den File nach den genauen Product durchsucht mithilfe von der Id, speichert dann die Ratings in einen temporären JsonArray, die dann in einen Long Array gespiechert werden und returned.
     public long[] getProdRatings(String prodid) throws IOException, ParseException {
         Path path = Paths.get("demo.json");
         Object json = new JSONParser().parse(new FileReader(path.toRealPath().toString() ));
